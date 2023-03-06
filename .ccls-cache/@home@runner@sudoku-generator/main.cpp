824b6d8804fb339112c0d673149f100b632b1c4e
Brandon Reed Gotcher <38430445+brgotcher@users.noreply.github.com>
2023-03-06 12:16:49 +0000

    implement backtracking algorithm to fill out the board accounting for invalid numbers

--- .ccls-cache/@home@runner@sudoku-generator/main.cpp
+++ .ccls-cache/@home@runner@sudoku-generator/main.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 void fill_diag_boxes(int[][9]);
-void fill_remainder(int[][9]);
+bool fill_remainder(int[][9], int, int);
 void fill_square(int[][9], int, int);
 bool h_valid(int[][9], int, int, int);
 bool v_valid(int[][9], int, int, int);
 bool s_valid(int[][9], int, int, int);
+bool is_valid(int[][9], int, int, int);
 void print_board(int[][9]);
 
 int main() {
@@ -25,14 +26,10 @@ int main() {
 	};
 	fill_diag_boxes(board);
 	print_board(board);
-	fill_remainder(board);
+	fill_remainder(board, 0, 3);
 
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			cout << board[i][j] << " ";
-		}
-		cout << endl;
-	}
+	cout << endl;
+	print_board(board);
 	
 }
 
@@ -57,30 +54,46 @@ void fill_diag_boxes(int board[][9]) {
 
 }
 
-void fill_remainder(int board[][9]) {
+bool fill_remainder(int board[][9], int row, int col) {
 	
-	for (int r = 0; r < 9; r+= 3) {
-		for (int c = 0; c < 9; c+=3) {
-			if (board[r][c] == 0) {
-				fill_square(board, r, c);
-				print_board(board);
+	// when end of row is reached, move to next row
+	if (col >= 9 && row < 9) {
+		col = 0;
+		row++;
+	}
+
+	// if the end of the grid has been reached, return true
+	if (col >= 9 && row >= 9) {
+		return true;
+	}
+
+	// if the current cel value is not zero, move to next box
+	if (board[row][col] != 0) {
+		col += 3;
+		if (col >= 9) {
+			col = 0; 
+			row++;
+			if (row >= 9) {
+				return true;
 			}
 		}
 	}
+	for (int i = 1; i < 10; i++) {
+		if (is_valid(board, row, col, i)) {
+			board[row][col] = i;
+			// print_board(board);
+			if (fill_remainder(board, row, col+1)) {
+				return true;
+			} else {
+				board[row][col] = 0;
+			}
+		}
+	}
+	return false;
 	
 }
 
-void fill_square(int board[][9], int row, int col) {
-	for (int r = row; r < row+3; r++) {
-		for (int c = col; c < col+3; c++) {
-			int val = rand() % 9 + 1;
-			while (!h_valid(board, r, c, val) || !v_valid(board, r, c, val) || !s_valid(board, r, c, val)) {
-				val = rand() % 9 + 1;
-			}
-			board[r][c] = val;
-		}
-	}
-}
+
 
 bool h_valid(int board[][9], int row, int col, int val) {
 	for (int i = 0; i < 9; i++) {
@@ -105,7 +118,7 @@ bool s_valid(int board[][9], int row, int col, int val) {
 	int left = col - (col % 3);
 	for (int r = top; r < (top+3); r++) {
 		for (int c = left; c < left+3; c++) {
-			if (board[r][c] == val) {
+			if (board[r][c] == val && !(r == row && c == col)) {
 				return false;
 			}
 		}
@@ -113,11 +126,29 @@ bool s_valid(int board[][9], int row, int col, int val) {
 	return true;
 }
 
+bool is_valid(int board[][9], int row, int col, int val) {
+	return (s_valid(board, row, col, val) && h_valid(board, row, col, val) && v_valid(board, row, col, val));
+}
+
 void print_board(int board[][9]) {
 	for (int i = 0; i < 9; i++) {
+		if (i % 3 == 0) {
+			cout << " ------------------------" << endl;
+		}
 		for (int j = 0; j < 9; j++) {
-			cout << board[i][j] << " ";
+			if (j % 3 == 0) {
+				cout << "| ";
+			}
+			cout << board[i][j];
+			if (j < 8) {
+				cout << " ";
+			} else {
+				cout << " |";
+			}
+		}
+		if ( i % 3 == 0) {
 		}
 		cout << endl;
 	}
+	cout << " ------------------------" << endl;
 }
